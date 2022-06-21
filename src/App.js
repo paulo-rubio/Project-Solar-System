fdc31b30a15c0f8ca6fec07b2c72e2febaab98c7
Rubio <paulocezarrubio@gmail.com>
2022-06-21 15:21:28 -0300

    req3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
+import SolarSistem from './components/Solar-Sistem';
+import Title from './components/Title';
 
 class App extends React.Component {
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <SolarSistem><Title /></SolarSistem>
+      </>
     );
   }
 }
---
+++ src/components/Solar-Sistem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SolarSistem extends Component {
+  render() {
+    return (
+      <div data-testid="solar-system"> </div>
+    );
+  }
+}
+
+export default SolarSistem;
