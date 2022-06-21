c61b73fbaa88ffded36824ecb1c5cf7a9331ff6e
Rubio <paulocezarrubio@gmail.com>
2022-06-21 15:42:13 -0300

    req 5

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PlanetCard extends Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <p testid="planet-name">{planetName}</p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+export default PlanetCard;
